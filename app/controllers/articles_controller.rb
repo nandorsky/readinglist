371ccb0b7c9b5c34e72d7567d88ad29e0374c8ba
Nate Andorsky <nandorsky@gmail.com>
2013-12-10 14:00:01 -0500

    finished back end dev

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
+    @article = current_user.articles.new
   end
 
   # GET /articles/1/edit
@@ -24,26 +24,22 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
+      redirect_to @article, notice: 'article was successfully created.'
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
   def update
     if @article.update(article_params)
-      redirect_to @article, notice: 'Article was successfully updated.'
+      redirect_to @article, notice: 'article was successfully updated.'
     else
       render action: 'edit'
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     @article.destroy
     redirect_to articles_url
@@ -55,8 +51,13 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
+    def correct_user
+      @article = current_user.articles.find_by(id: params[:id])
+      redirect_to articles_path, notice: "Not authorized to edit this article" if @article.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:description)
+      params.require(:article).permit(:description, :image)
     end
 end
